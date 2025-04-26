9c756bd45512a455d8e3c572dbd75f94a237bde2
anshuman0se <147651313+anshuman0se@users.noreply.github.com>
2025-04-25 21:49:31 -0400

    Update tracing.js

--- tracing.js
+++ tracing.js
@@ -8,15 +8,12 @@ const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventi
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
 
 const sdk = new NodeSDK({
-  // 1) Name your service “todo-service” so Jaeger can list it
   resource: resourceFromAttributes({
     [SemanticResourceAttributes.SERVICE_NAME]: 'todo-service'
   }),
-  // 2) Send spans to Jaeger’s OTLP HTTP endpoint
   traceExporter: new OTLPTraceExporter({
     url: 'http://localhost:4318/v1/traces'
   }),
-  // 3) Auto-instrument HTTP, Express, MongoDB, etc.
   instrumentations: [
     getNodeAutoInstrumentations()
   ],
